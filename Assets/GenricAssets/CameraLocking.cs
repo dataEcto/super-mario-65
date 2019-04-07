b041528284b67b28ada1582450e39d3fcea5d9f1
Genric <gj.elazegui@Gmail.com>
2019-04-07 17:41:25 -0400

    Movement

---
+++ Assets/GenricAssets/CameraLocking.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLocking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
