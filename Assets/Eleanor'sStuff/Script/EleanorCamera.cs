db1346b4eab91f5142e59bb26a36cd69433f2a7e
Jiaxi Yang <jy2134@nyu.edu>
2019-04-08 23:17:01 -0400

    test Movement

---
+++ Assets/Eleanor'sStuff/Script/EleanorCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EleanorCamera : MonoBehaviour
+{
+    public GameObject ObjToFollow;
+
+    private Vector3 _offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ObjToFollow != null)
+            _offset = transform.position - ObjToFollow.transform.position;
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (ObjToFollow == null) return;
+        transform.LookAt(ObjToFollow.transform.position);
+            transform.position = Vector3.Lerp(transform.position, ObjToFollow.transform.position + _offset, 0.1f);
+    }
+}
---
+++ Assets/Eleanor'sStuff/Script/EleanorMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EleanorMovement : MonoBehaviour
+{
+
+    private Rigidbody _rb;
+    private bool _walk;
+    private bool _walkAudioPlaying;
+    private bool _jump;
+    private bool _end;
+       
+    
+    public float MouseSensitivity;
+    public float MoveSpeed;
+    public float JumpForce;
+
+
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    
+    void Update ()
+    {
+               
+        _rb.MoveRotation(_rb.rotation * Quaternion.Euler(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0));
+        _rb.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * MoveSpeed) + (transform.right * Input.GetAxis("Horizontal") * MoveSpeed));
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * JumpForce);
+            _jump = true;
+        }
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_jump)
+        {
+            _jump = false;
+        }
+    }
+}
+
