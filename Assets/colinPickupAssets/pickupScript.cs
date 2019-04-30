27ced1edcaa90eb27915e77303463127bd49ae91
LAPTOP-1L0MPP9Q\demok <ch3067@nyu.edu>
2019-04-30 10:19:45 -0400

    Safe recovery branch

--- Assets/colinPickupAssets/pickupScript.cs
+++ Assets/colinPickupAssets/pickupScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -9,9 +10,14 @@ public class pickupScript : MonoBehaviour
     public int coins;
 
     public int stars;
+    public int lives;
     public Canvas canvas;
     public TextMeshProUGUI _coinTextMeshProUgui;
     public TextMeshProUGUI _starTextMeshProUgui;
+    public TextMeshProUGUI _MarioLivesTextMeshProUgui;
+
+    public String[] spriteArray;
+    
 
     public GameObject Star;
 
@@ -21,7 +27,8 @@ public class pickupScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      
+        setArray();
+        lives = 3;
     }
 
     // Update is called once per frame
@@ -29,8 +36,9 @@ public class pickupScript : MonoBehaviour
     {
         updateCoins();
         updateStars();
+        updateLives();
     }
-
+// Triggers for coins, stars, and killzones
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("star"))
@@ -49,37 +57,81 @@ public class pickupScript : MonoBehaviour
             other.gameObject.SetActive(false);
             
         }
+
+        if (other.gameObject.CompareTag("Killzone"))
+        {
+            lives = lives - 1;
+        }
     }
 
     void updateCoins()
     {
-        _coinTextMeshProUgui.text = coins.ToString();
+        _coinTextMeshProUgui.text = spriteArray[coins].ToString();
+        Debug.Log(coins);
+        if (coins>=10)
+        {
+            numbersAbove9();
+        }
     }
 
     void updateStars()
     {
-        _starTextMeshProUgui.text = stars.ToString();
+        _starTextMeshProUgui.text = spriteArray[stars].ToString();
+        Debug.Log(stars);
+        if (stars >= 10)
+        {
+            numbersAbove9();
+        }
     }
 
-
-
-    void OnTriggerEnter(Collider other)
+    void updateLives()
     {
-        if(other.gameObject.tag == "star" && other.isTrigger == true) {
+        _MarioLivesTextMeshProUgui.text = spriteArray[lives].ToString();
+        Debug.Log(lives);
+    }
+//array holds sprite draw strings for TMPro
+    void setArray()
+    {
+        spriteArray[0] = "<sprite index = 0>";
+        spriteArray[1] = "<sprite index = 1>";
+        spriteArray[2] = "<sprite index = 2>";
+        spriteArray[3] = "<sprite index = 3>";
+        spriteArray[4] = "<sprite index = 4>";
+        spriteArray[5] = "<sprite index = 5>";
+        spriteArray[6] = "<sprite index = 6>";
+        spriteArray[7] = "<sprite index = 7>";
+        spriteArray[8] = "<sprite index = 8>";
+        spriteArray[9] = "<sprite index = 9>";
+    }
+//Numbers above 9 adds sprite combinations to cover all numbers
+    void numbersAbove9()
+    {
+        if (stars == 10)
+        {
+            _starTextMeshProUgui.text = spriteArray[1] + spriteArray[9];
+        }
 
-            thisAnimator.Play("StarPickedUp");
-            Invoke("StarDisappear", 1.3f);
+        if (coins == 10)
+        {
+            _coinTextMeshProUgui.text = spriteArray[1] + spriteArray[9];
+        }
+        if (stars == 11)
+        {
+            _starTextMeshProUgui.text = spriteArray[0] + spriteArray[0];
+        }
 
-           
-             }
+        if (coins == 11)
+        {
+            _coinTextMeshProUgui.text = spriteArray[1] + spriteArray[1];
+        }
+
+        if (coins == 12)
+        {
+            _coinTextMeshProUgui.text = spriteArray[0] + spriteArray[1];
+        }
     }
 
 
 
-    void StarDisappear() {
 
-        Star.SetActive(false);
-      
-       
-         }
 }
