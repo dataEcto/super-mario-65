6af903d883c3f25c7423edd1e5f4b91749eeaa3c
jiaxiyang <jy2134@nyu.edu>
2019-05-07 11:51:50 -0400

    Master Scene update

--- Assets/Eleanor'sStuff/Script/Trigger.cs
+++ Assets/Eleanor'sStuff/Script/Trigger.cs
@@ -6,40 +6,12 @@ using UnityEditor;
 
 public class Trigger : MonoBehaviour
 {
-<<<<<<< HEAD
-    public CharacterController PlayerController;
-    public MasterMovementEleanorTest MovementScriptEleanorTest;//Replace Genricmovement with MasterMovement
-    
     public Animator Anim;
-=======
-       
-    // Camera List
-    public GameObject PreviousCamera;
-    public GameObject NewCamera;
-
-
-
-
-
-//    public MasterMovement.Movement LocalMovementMode;
-
-
-    //public bool LockIntention;
-//    private CinemachineBrain brain;
-
-    /*public enum TransitionMode
-    {
-        Blend,
-        CutScene,      
-    }
->>>>>>> f718c79580b725770c82ca49c9d031cd9e031b27
-
     public bool Slide;
 
     private void OnTriggerExit(Collider other)
     {
-
-        MovementScriptEleanorTest.OnSlide = Slide;
+        UpdatedMasterMovement.Singleton.OnSlide = Slide;
         Anim.SetBool("Slide", Slide);
     }
 
