209c7706604405ec09484c7e6daa3f644f75233f
JunLin Li <jl8278@nyu.edu>
2019-05-07 03:24:00 -0400

    Broken Coin Audio

---
+++ Assets/MasterScript/AudioCarriere.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class AudioCarriere : MonoBehaviour
+{
+
+
+    public AudioSource marioSoundEffect;
+
+    //Pick up script sound effect
+    public AudioClip coinPickUpClip;
+    public AudioClip starPickupClip;
+    public AudioClip deathClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        marioSoundEffect = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pickupScript.starPickUpSound == true) {
+
+            marioSoundEffect.PlayOneShot(starPickupClip);
+         
+           }
+
+        if(pickupScript.coinPickUpSound == true) {
+
+            marioSoundEffect.PlayOneShot(coinPickUpClip);
+        }
+       
+
+        if (pickupScript.deathSound == true) {
+
+            marioSoundEffect.PlayOneShot(deathClip);
+          }
+    }
+}
